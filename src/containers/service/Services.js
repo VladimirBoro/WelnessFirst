53f2dea7f65d743d85858b2e548848a24aa13da6
VladimirBoro <vborodianski19@gmail.com>
2025-03-11 21:25:27 -0700

    small site side style updates/changes

--- src/containers/service/Services.js
+++ src/containers/service/Services.js
@@ -7,7 +7,7 @@ import styles from "./services.module.css";
 function Services() {
     return (
         <div id={styles.servicePage}>
-            <h1 id={styles.title}>Our Services</h1>
+            <h1 id={styles.title}>Services</h1>
             <div id={styles.contact}>
                 <p style={{marginTop: "0"}}>By Appointment only</p>
                 <Link to="/contact">
