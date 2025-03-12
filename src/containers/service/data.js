c4298874a3a8b4bfe865333194d9a709ac59e0b8
VladimirBoro <vborodianski19@gmail.com>
2025-03-11 21:21:41 -0700

    updated service pricing values

--- src/containers/service/data.js
+++ src/containers/service/data.js
@@ -2,57 +2,52 @@ const data = [
     {
         title: "Deep Tissue Massage",
         description: "Great for pain relief, hard pressure strokes designed to work not only with your muscles, but reach the fascia and connective tissue.",
-        prices: [150, 210]
-    },
-    {
-        title: "Swedish Massage",
-        description: "This traditional technique combines a variety of classic movements to provide the Client with calming stress relief. It will improve blood circulation and enhance the level of physical performance.",
-        prices: [120, 180]
+        prices: [170, 250]
     },
     {
         title: "Neuromuscular Therapy",
         description: "Massage used to assist the repair of soft tissue function through neurological laws starting from the point of injury to pain.",
-        prices: [150, 210]
+        prices: [180, 260]
     },
     {
         title: "Shiatsu",
         description: "A combination of special massage techniques to stimulate 12 organ meridians running through our body. Meridians are energy pathways through which Qi flows. Qi - Life Energy. A perfect treatment for specific problems to restore balance of mind, body and soul.",
-        prices: [150, 210]
+        prices: [170, 250]
     },
     {
         title: "Hot Stone Massage",
         description: "This wonderful technique concentrates the application of the heated smooth stones soaked in warm oils to specific points on your back. Long flowing strokes are applied to ensure deep muscle relief and a perfect sense of calm and balance.",
-        prices: [160, 220]
+        prices: [180, 260]
     },
     {
         title: "Cupping",
         description: "Therapy is the method of using plastic cups to create localized pressure by a vacuum. The vacuum inside the cups causes the blood to form in the area and help the healing in that area. It triggers the lymphatic system, clears the blood vessels, and stretches and activates the skin.",
-        prices: [150, 210]
+        prices: [170, 250]
     },
     {
         title: "Aromatherapy",
         description: "A combination of carefully selected essential oils blended to provide nourishment for your skin. The aromatic essence will soothe any pain and calm your nerves. Healing powers of plans and  flowers essences promote relaxation and well-being.",
-        prices: [140, 190]
+        prices: [160, 250]
     },
     {
         title: "Reiki",
         description: "Is a simple, natural and safe method of spiritual healing. It has been effective in helping virtual every known illness and malady. Used in combination with therapeutic massage techniques, it offers alternate benefits to promote relief and recovery.",
-        prices: [150, 210]
+        prices: [180, 260]
     },
     {
         title: "Pregnancy",
         description: "Carefully designed to release tension, improve circulation, relax, support your entire body, while focusing on the legs. Your therapist will use safe techniques for pregnancy massage.",
-        prices: [150, 210]
+        prices: [160, 250]
     },
     {
         title: "Lymphatic Drainage",
         description: "Provides a mechanical means of inducing the lymph to flow in the right direction. You could transform the look of the skin, the form of your body and your Health.",
-        prices: [160, 220]
+        prices: [180, 260]
     },
     {
-        title: "Custom Massage",
-        description: "It's a combination of various techniques, including Hot Stone Therapy, Yoga techniques,  Neuromuscular therapy, Aromatherapy, Reiki, Deep Tissue and Hot Towels. It's a perfect massage to relieve stress and restore balance, increase energy flow, and loosen tight muscles!",
-        prices: [160, 230]
+        title: "Signature Massage",
+        description: "It's a combination of various techniques, including Hot Stone Therapy, Neuromuscular therapy, Aromatherapy, Reiki, Deep Tissue, Cupping, and Hot Towels. It's a perfect massage to relieve stress and restore balance, increase energy flow, and loosen tight muscles!",
+        prices: [180, 260]
     },
 ]
 
